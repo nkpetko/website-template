491b29f0d875fd8edc60f3d483c00719c0d0c4d7
ROBLOX Team Foundation <github@roblox.com>
2021-03-24 18:53:44 +0000

    Define the base directory here, because __dirname is useless in this useage.

---
+++ Assemblies/Util/Directories.ts
@@ -0,0 +1,4 @@
+// Base Directory
+export const _baseDirName = 'C:\\Your\\Path\\Here'; // Must have \\, not \ or /
+// This contains some certs and private keys
+export const _sslDirName = _baseDirName + '\\SSL';
